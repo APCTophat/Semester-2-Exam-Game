a6b1a2e05972b9f4d5c8078454164b542d84817f
APCTophat <49924900+APCTophat@users.noreply.github.com>
2019-10-22 23:19:59 +0200

    SetUp Scenemanagement

---
+++ 2nd year_Semester 2_Exam game/Assets/sceneManagerScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class sceneManagerScript : MonoBehaviour
+{
+
+    public string GameSceneName;
+    public string ControlsScene;
+    public string MenuSceneName;
+    public string EndCreditSceneName;
+
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void LoadGameScene()
+    {
+        SceneManager.LoadScene(GameSceneName);
+    }
+
+    public void LoadCrontrolsScene()
+    {
+        SceneManager.LoadScene(ControlsScene);
+    }
+
+    public void LoadMenuScene()
+    {
+        SceneManager.LoadScene(MenuSceneName);
+    }
+
+    public void LoadEndCreditScene()
+    {
+        SceneManager.LoadScene(EndCreditSceneName);
+    }
+}
